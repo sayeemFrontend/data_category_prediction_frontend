5616b8cc21aa17c0f0fd552ec600dc1aa7004351
unknown <amanullahsayeem@gmail.com>
2023-10-13 01:28:35 +0600

    add: validation to input

--- src/componets/LinkForm.jsx
+++ src/componets/LinkForm.jsx
@@ -1,11 +1,10 @@
 import './style.css';
 import PropTypes from 'prop-types';
-
 export default function LinkForm({ handleSubmit = () => {} }) {
   return (
     <div className='insert-form'>
       <form onSubmit={handleSubmit}>
-        <input required name='url' type='text' placeholder='Insert news link' />
+        <input required pattern='https?://.+' name='url' title='Please Insert valid input' type='text' placeholder='Insert news link' />
         <button className='btn-normal' type='submit'>
           Submit
         </button>
